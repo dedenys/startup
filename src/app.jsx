a949f11afddf779e2f29ffd40f48a8444a180fad
dedenys <dedenys.school@gmail.com>
2024-11-06 10:14:49 -0700

    added router

--- src/app.jsx
+++ src/app.jsx
@@ -10,6 +10,7 @@ import { Calender } from './calender/calender';
 
 export default function App() {
   return (
+    <BrowserRouter>
   <div className="bg-secondary text-dark bg-light">
     <div className="container">
       <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-0 ">
@@ -18,10 +19,23 @@ export default function App() {
         </a>
   
         <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="index.html" className="nav-link px-3 link-secondary">Home</a></li>
+          {/*<li><a href="index.html" className="nav-link px-3 link-secondary">Home</a></li>
           <li><a href="calender.html" className="nav-link  px-3 ">MyCalender</a></li>
           <li><a href="concepts.html" className="nav-link px-3 ">Add/View Concepts</a></li>
-          <li><a href="tally.html" className="nav-link px-3 ">Global Tally</a></li>
+          <li><a href="tally.html" className="nav-link px-3 ">Global Tally</a></li> */}
+          <li>
+            <NavLink className='nav-link' to=''>Login</NavLink>
+          </li>
+          <li>
+            <NavLink className='nav-link' to='concepts'>Concepts</NavLink>
+          </li>
+          <li>
+            <NavLink className='nav-link' to='tally'>Tally</NavLink>
+          </li>
+          <li>
+           <NavLink className='nav-link' to='calender'>Calender</NavLink>
+          </li>
+
         </ul>
   
         <div className="col-md-3 text-end">
@@ -30,7 +44,13 @@ export default function App() {
       </header>
     </div>
 
-    <main>main content here</main>
+    <Routes>
+      <Route path='/' element={<Login />} exact />
+      <Route path='/concepts' element={<Concepts />} />
+      <Route path='/tally' element={<Tally />} />
+      <Route path='/calender' element={<Calender />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
 
     <footer className="container">
       <span className="text-reset">Derek Denys</span>
@@ -38,5 +58,10 @@ export default function App() {
     </footer>
 
   </div>
+  </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
 }
