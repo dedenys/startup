86c082b7f24f5990ce2a47ca6e9157b73246a380
dedenys <dedenys.school@gmail.com>
2024-11-16 12:08:31 -0700

    vite config

--- service/index.js
+++ service/index.js
@@ -12,6 +12,8 @@ const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 app.use(express.json());
 
+app.use(express.static('public'));
+
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
