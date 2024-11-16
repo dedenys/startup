7edf727f56e0757ca2c5a247f6763222ebd04603
dedenys <dedenys.school@gmail.com>
2024-11-15 21:10:00 -0700

    index.js modified

--- service/index.js
+++ service/index.js
@@ -2,7 +2,17 @@ const express = require('express');
 const uuid = require('uuid');
 const app = express();
 
+let users = {};
+let concepts = [];
+let tally = 0;
+
+
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+app.use(express.json());
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
 
 app.get('*', (_req, res) => {
   res.send({ msg: 'Simon service' });
