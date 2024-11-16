0c8549368663950c6010a65d878e9463426ed4f9
dedenys <dedenys.school@gmail.com>
2024-11-15 21:02:14 -0700

    index.js modified

--- service/index.js
+++ service/index.js
@@ -11,11 +11,3 @@ app.get('*', (_req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-Now we can run the application with node index.js and hit the service with Curl.
-
-node index.js &
-curl localhost:3000
-
-{"msg":"Simon service"}
-
-# use fg to foreground and kill the process
