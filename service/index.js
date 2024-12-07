2f2b4bcdc6e9f72601591d3f565135a380102200
dedenys <dedenys.school@gmail.com>
2024-12-07 11:54:04 -0700

    attaching websocket to HTTP

--- service/index.js
+++ service/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const uuid = require('uuid');
 const app = express();
 const DB = require('./database.js');
+const { peerProxy } = require('./peerProxy.js');
 
 const authCookieName = 'token';
 
@@ -273,3 +274,9 @@ function updateConcepts(newConcept, concepts) {
 
   return concepts;
 }
+
+const httpService = app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+peerProxy(httpService);
